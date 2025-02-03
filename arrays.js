fc7cfe1bd52ac4fd85c76db3d430169e297c308a
Patrick <patrickmassesson@gmail.com>
2025-02-03 09:31:04 +0100

    opg

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let catsArray = ["Ollie", "Sophie", "Salem", "Tiger", "Binx", "Pumpkin", "Penny", "Lenny", "Sniffles"];
     let dogsArray = ["Barney", "Molly", "Baxter", "Polly", "Buddy", "Suki", "Watson"];
     let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
@@ -9,16 +9,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Opgave 1: Lav en ny array med alle tal fra numbersArray, som er større end 20
 
-    let newNumbers = numbersArray.filter(function(number) {
+    let newNumbers = numbersArray.filter(function (number) {
         return number > 20
     })
     console.log("Opgave 1", newNumbers)
 
-
-
     // opgave 2 Tilføj katten "Sniffles" til catsArray.
     console.log(catsArray)
-    
+
 
     //opgave 3 a Check om der i dogsArray er en hund som hedder "Trixie".
     let trixie = dogsArray.includes("Trixie")
@@ -37,11 +35,11 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log(salem)
 
     //opgave 5 Med udgangspunkt i numbersArray, skal du oprette et nyt array, som indeholder alle tallene i arrayet ganget med 3.
-    let newNumbersArray = numbersArray.map(function(number) {
+    let newNumbersArray = numbersArray.map(function (number) {
         return number * 3
     })
     console.log(newNumbersArray)
-    
+
     //opgave 6 Udskriv alle hundene i dogsArray i én lang streng. (Sådan her: BarneyMollyBaxter...)
     let dogsString = dogsArray.join("")
     console.log(dogsString)
@@ -54,7 +52,8 @@ document.addEventListener("DOMContentLoaded", function() {
     let BlåbærIndex = fruitsArray.indexOf("Blåbær")
     console.log(BlåbærIndex)
 
-
+    //opgave 8 Lad os forestille os, at dogsArray er en liste af tilmeldte hunde til en udstilling. Den familie der har hunden "Polly" er desværre blevet forhindret i at deltage i udstillingen. Fjern hunden "Polly" fra dogsArray og udskriv det nye array i konsollen
+    let pollyIndex = dogsArray.indexOf("Polly")
 
 
 
